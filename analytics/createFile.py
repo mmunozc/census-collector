c47622d4810d13c8cf93fbffdfbda6bf7af82910
Jose Manuel <jmcamargoh@eafit.edu.co>
2023-11-21 23:27:37 -0500

    Recepción total

--- analytics/createFile.py
+++ analytics/createFile.py
@@ -14,17 +14,25 @@ def createExcel(data):
             adressSectionSheet.append([value['CFN'], value['adressSection']['Q1'], value['adressSection']['Q2'], value['adressSection']['Q3']])
 
         if value.get('personSection'):
-            personSectionSheet.append([value['CFN']] + [value['personSection'].get('Page1', {}).get(f'Q{i}', '') for i in range(1, 21)])
+            personSectionSheet.append([value['CFN']] + [value['personSection'].get('Page1', {}).get(f'Q{i}', '') for i in range(1, 4)] + 
+                                      [value['personSection'].get('Page2', {}).get(f'Q{i}', '') for i in range(4, 7)] +
+                                      [value['personSection'].get('Page3', {}).get(f'Q{i}', '') for i in range(7, 10)] +
+                                      [value['personSection'].get('Page4', {}).get(f'Q{i}', '') for i in range(10, 13)] +
+                                      [value['personSection'].get('Page5', {}).get(f'Q{i}', '') for i in range(13, 16)] +
+                                      [value['personSection'].get('Page6', {}).get(f'Q{i}', '') for i in range(16, 19)] +
+                                      [value['personSection'].get('Page7', {}).get(f'Q{i}', '') for i in range(19, 22)])
 
         if value.get('dwellingSection'):
-            dwellingSectionSheet.append([value['CFN']] + [value['dwellingSection'].get('Page1', {}).get(f'Q{i}', '') for i in range(1, 10)])
+            dwellingSectionSheet.append([value['CFN']] + [value['dwellingSection'].get('Page1', {}).get(f'Q{i}', '') for i in range(1, 4)] +
+                                        [value['dwellingSection'].get('Page2', {}).get(f'Q{i}', '') for i in range(4, 7)] +
+                                        [value['dwellingSection'].get('Page3', {}).get(f'Q{i}', '') for i in range(7, 10)])
 
         if value.get('feedbackSection'):
             feedbackSectionSheet.append([value['CFN']] + [value['feedbackSection'].get(f'Q{i}', '') for i in range(1, 4)])
 
     dfFirtsSheet = pd.DataFrame(firtsSheet, columns=['ID ECN', 'CFN', 'State', 'Completed Pages'])
     dfAdressSectionSheet = pd.DataFrame(adressSectionSheet, columns=['CFN'] + [f'Q{i}' for i in range(1, 4)])
-    dfPersonSectionSheet= pd.DataFrame(personSectionSheet, columns=['CFN'] + [f'Q{i}' for i in range(1, 21)])
+    dfPersonSectionSheet= pd.DataFrame(personSectionSheet, columns=['CFN'] + [f'Q{i}' for i in range(1, 22)])
     dfDwellingSectionSheet = pd.DataFrame(dwellingSectionSheet, columns=['CFN'] + [f'Q{i}' for i in range(1, 10)])
     dFfeedbackSectionSheet = pd.DataFrame(feedbackSectionSheet, columns=['CFN'] + [f'Q{i}' for i in range(1, 4)])
 
