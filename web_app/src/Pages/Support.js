a7de098392b08ab8637e2b3f0c4ba9f6d429b9a3
Paulina-Lopez <lopezpaulina22@gmail.com>
2023-11-23 14:24:46 -0500

    Detalles en front

--- web_app/src/Pages/Support.js
+++ web_app/src/Pages/Support.js
@@ -8,7 +8,7 @@ function Support() {
             <div className="left-pane">
                 <div className="button-container">
                     <h1 style={{ fontWeight: 'bold'}}>Support Center</h1>
-                    <p>Contact us through any of the platforms below for support.</p>
+                    <p>Contact us through any of the platforms below for support and assistance.</p>
                     <a href="https://wa.me/3003843232" className="round-button" target="_blank" rel="noopener noreferrer">WhatsApp</a><br></br>
                     <a href="tel:+3003843232" className="round-button">Call Us</a><br></br>
                     <a href="mailto:support@bolumbia.com" className="round-button" target="_blank" rel="noopener noreferrer">Email Us</a>
