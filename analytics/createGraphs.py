1f3ea2f56e152913903411398be475330793728d
Jose Manuel Camargo <jmcamargoh@eafit.edu.co>
2023-11-22 21:13:56 -0500

    Gráfica de Pastel

--- analytics/createGraphs.py
+++ analytics/createGraphs.py
@@ -19,6 +19,13 @@ def graph():
         plt.tight_layout()
         plt.show()
     
+    def pieplot(count_answers, title):
+        plt.figure(figsize=(8,8))
+        plt.pie(count_answers, labels=count_answers.index, autopct='%1.1f%%', startangle=140, colors=['skyblue', 'lightgreen'])
+        plt.title(title)
+        plt.axis('equal')
+        plt.tight_layout()
+        plt.show()
 
     def graph_AdressSection():
         data_adressSection = pd.read_excel(excel_file, sheet_name=adressSectionSheet)
@@ -27,24 +34,10 @@ def graph():
         barplot(count_column, 'Cantidad de Personas que viven en el Hogar', 'Cantidad de Personas', 'Frecuencia')
 
     def graph_PersonSection():
-        # Leer el archivo y la hoja deseada
         data_personSection = pd.read_excel(excel_file, sheet_name=personSectionSheet)
-
-        # Acceder a una columna específica
         column_q1 = data_personSection['Q1']
-        # Filtrar las respuestas
-        q1_si_no = data_personSection[data_personSection['Q1'].isin(['Si','No'])]
-        # Total de valores en la columna
         count_column_q1 = column_q1.value_counts()
-
-        # Crear un gráfico de pastel para ver las respuestas
-        plt.figure(figsize=(8,8))
-        plt.pie(count_column_q1, labels=count_column_q1.index, autopct='%1.1f%%', startangle=140, colors=['skyblue', 'lightgreen'])
-        plt.title('Distribución de Respuestas "Sí" y "No" para Q1 en Person Section')
-        plt.axis('equal')
-        plt.tight_layout()
-        plt.savefig('pastel_q1.png')    # Para guardar el gráfico generado como una imagen
-        plt.show()
+        pieplot(count_column_q1, 'Q1')
 
 
     graph_PersonSection()
