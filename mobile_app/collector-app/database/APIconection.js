c6449b3721632200556c57825a81ac9006925993
pmayavi <pmayav@eafit.edu.co>
2023-11-22 20:16:36 -0500

    Conexion de datos local

--- mobile_app/collector-app/database/APIconection.js
+++ mobile_app/collector-app/database/APIconection.js
@@ -1,59 +1,59 @@
-var mainJson = {
-    "050000111": {
-        "00000101110": {
-            "direction": "Carrera 45 # 123 - 56, Medellín",
-            "state": false,
-            "ECN": "438525040000"
-        },
-        "00000201110": {
-            "direction": "Calle 70 # 45 - 89, Medellín",
-            "state": false,
-            "ECN": "739510630000"
-        },
-        "00000301110": {
-            "direction": "Avenida 80 # 34 - 67, Medellín",
-            "state": false,
-            "ECN": "294170420000"
-        }
-    },
-    "050010222": {
-        "00000102220": {
-            "direction": "Calle 12 # 345, Medellín",
-            "state": false,
-            "ECN": "050004"
-        },
-        "00000202220": {
-            "direction": "Carrera 25 # 678, Medellín",
-            "state": false,
-            "ECN": "050005"
-        },
-        "00000302220": {
-            "direction": "Avenida 50 # 901, Medellín",
-            "state": false,
-            "ECN": "050006"
-        }
-    },
-    "050020333": {
-        "00000403330": {
-            "direction": "Carrera 60 # 234, Medellín",
-            "state": false,
-            "ECN": "050007"
-        },
-        "00000503330": {
-            "direction": "Calle 85 # 789, Medellín",
-            "state": false,
-            "ECN": "050008"
-        },
-        "00000603330": {
-            "direction": "Avenida 95 # 012, Medellín",
-            "state": false,
-            "ECN": "050009"
-        }
-    }
-}
+var mainJson = {};
 
 const StartConection = async () => {
-    mainJson = await readLocalJsonFile("dwellings.json");
+    mainJson = {
+        "050000111": {
+            "00000101110": {
+                "direction": "Carrera 45 # 123 - 56, Medellín",
+                "state": false,
+                "ECN": "438525040000"
+            },
+            "00000201110": {
+                "direction": "Calle 70 # 45 - 89, Medellín",
+                "state": false,
+                "ECN": "739510630000"
+            },
+            "00000301110": {
+                "direction": "Avenida 80 # 34 - 67, Medellín",
+                "state": false,
+                "ECN": "294170420000"
+            }
+        },
+        "050010222": {
+            "00000102220": {
+                "direction": "Calle 12 # 345, Medellín",
+                "state": false,
+                "ECN": "050004"
+            },
+            "00000202220": {
+                "direction": "Carrera 25 # 678, Medellín",
+                "state": false,
+                "ECN": "050005"
+            },
+            "00000302220": {
+                "direction": "Avenida 50 # 901, Medellín",
+                "state": false,
+                "ECN": "050006"
+            }
+        },
+        "050020333": {
+            "00000403330": {
+                "direction": "Carrera 60 # 234, Medellín",
+                "state": false,
+                "ECN": "050007"
+            },
+            "00000503330": {
+                "direction": "Calle 85 # 789, Medellín",
+                "state": false,
+                "ECN": "050008"
+            },
+            "00000603330": {
+                "direction": "Avenida 95 # 012, Medellín",
+                "state": false,
+                "ECN": "050009"
+            }
+        }
+    };
 }
 
 const GetZoneData = async (user) => {
@@ -65,9 +65,7 @@ const SendUpdate = async (user, item) => {
 }
 
 const ValidateUser = async (user) => {
-    const data = await readLocalJsonFile("dwellings.json");
-    //console.log("data.hasOwnProperty(user)", data.hasOwnProperty(user));
-    return data.hasOwnProperty(user);
+    return mainJson.hasOwnProperty(user);
 }
 
 export { GetZoneData, ValidateUser, SendUpdate, StartConection };
