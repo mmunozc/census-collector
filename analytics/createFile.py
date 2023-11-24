572a5d098e2ab4d36d678d900d3e9b495024f13f
pmayavi <pmayav@eafit.edu.co>
2023-11-23 21:39:57 -0500

    Update createFile.py

--- analytics/createFile.py
+++ analytics/createFile.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 
+
 def createExcel(data, folder):
     if not os.path.exists(folder):
         os.mkdir(folder)
@@ -12,40 +13,88 @@ def createExcel(data, folder):
     feedbackSectionSheet = []
 
     for key, value in data.items():
-        firtsSheet.append([key, value.get('CFN', ''), value.get('state', ''), value.get('completedPages', '')])
+        firtsSheet.append(
+            [
+                key,
+                value.get("CFN", ""),
+                value.get("state", ""),
+                value.get("completedPages", ""),
+            ]
+        )
 
-        if value.get('adressSection'):
-            adressSectionSheet.append([value['CFN'], value['adressSection']['Q1'], value['adressSection']['Q2'], value['adressSection']['Q3']])
+        if value.get("adressSection"):
+            adress_section_values_list = [value["CFN"]]
+            adress_section_values = value["adressSection"].values()
+            for question in adress_section_values:
+                adress_section_values_list.append(question)
+            adressSectionSheet.append(adress_section_values_list)
 
-        if value.get('personSection'):
-            personSectionSheet.append([value['CFN']] + [value['personSection'].get('Page1', {}).get(f'Q{i}', '') for i in range(1, 4)] + 
-                                        [value['personSection'].get('Page2', {}).get(f'Q{i}', '') for i in range(4, 7)] +
-                                        [value['personSection'].get('Page3', {}).get(f'Q{i}', '') for i in range(7, 10)] +
-                                        [value['personSection'].get('Page4', {}).get(f'Q{i}', '') for i in range(10, 13)] +
-                                        [value['personSection'].get('Page5', {}).get(f'Q{i}', '') for i in range(13, 16)] +
-                                        [value['personSection'].get('Page6', {}).get(f'Q{i}', '') for i in range(16, 19)] +
-                                        [value['personSection'].get('Page7', {}).get(f'Q{i}', '') for i in range(19, 22)])
+        if value.get("personSection"):
+            person_section_values_list = [value["CFN"]]
+            person_section_values = value["personSection"].values()
+            for page in person_section_values:
+                for question in page.values():
+                    person_section_values_list.append(question)
+            personSectionSheet.append(person_section_values_list)
 
-        if value.get('dwellingSection'):
-            dwellingSectionSheet.append([value['CFN']] + [value['dwellingSection'].get('Page1', {}).get(f'Q{i}', '') for i in range(1, 4)] +
-                                        [value['dwellingSection'].get('Page2', {}).get(f'Q{i}', '') for i in range(4, 7)] +
-                                        [value['dwellingSection'].get('Page3', {}).get(f'Q{i}', '') for i in range(7, 10)])
+        if value.get("dwellingSection"):
+            dwelling_section_values_list = [value["CFN"]]
+            dwelling_section_values = value["dwellingSection"].values()
+            for page in dwelling_section_values:
+                for question in page.values():
+                    dwelling_section_values_list.append(question)
+            dwellingSectionSheet.append(dwelling_section_values_list)
 
-        if value.get('feedbackSection'):
-            feedbackSectionSheet.append([value['CFN']] + [value['feedbackSection'].get(f'Q{i}', '') for i in range(1, 4)])
+        if value.get("feedbackSection"):
+            feedback_section_values_list = [value["CFN"]]
+            feedback_section_values = value["feedbackSection"].values()
+            for question in feedback_section_values:
+                feedback_section_values_list.append(question)
+            feedbackSectionSheet.append(feedback_section_values_list)
 
-    dfFirtsSheet = pd.DataFrame(firtsSheet, columns=['ID ECN', 'CFN', 'State', 'Completed Pages'])
-    dfAdressSectionSheet = pd.DataFrame(adressSectionSheet, columns=['CFN'] + [f'Q{i}' for i in range(1, 4)])
-    dfPersonSectionSheet= pd.DataFrame(personSectionSheet, columns=['CFN'] + [f'Q{i}' for i in range(1, 22)])
-    dfDwellingSectionSheet = pd.DataFrame(dwellingSectionSheet, columns=['CFN'] + [f'Q{i}' for i in range(1, 10)])
-    dFfeedbackSectionSheet = pd.DataFrame(feedbackSectionSheet, columns=['CFN'] + [f'Q{i}' for i in range(1, 4)])
+    dfFirtsSheet = pd.DataFrame(
+        firtsSheet, columns=["ID ECN", "CFN", "State", "Completed Pages"]
+    )
 
-    excelPath = os.path.join(folder, 'stats.xlsx')
-    with pd.ExcelWriter(excelPath, engine='xlsxwriter') as writer:
-        dfFirtsSheet.to_excel(writer, sheet_name='Sections', index=False)
-        dfAdressSectionSheet.to_excel(writer, sheet_name='AdressSection', index=False)
-        dfPersonSectionSheet.to_excel(writer, sheet_name='PersonSection', index=False)
-        dfDwellingSectionSheet.to_excel(writer, sheet_name='DwellingSection', index=False)
-        dFfeedbackSectionSheet.to_excel(writer, sheet_name='FeedbackSection', index=False)
-    
+    addressKeys = list(data[list(data.keys())[0]]["adressSection"].keys())
+    dfAdressSectionSheet = pd.DataFrame(
+        adressSectionSheet, columns=["CFN"] + [f"{i}" for i in addressKeys]
+    )
+
+    personKeys = list(data[list(data.keys())[0]]["personSection"].keys())
+    for page in list(data[list(data.keys())[0]]["personSection"].keys()):
+        personKeys.append
+    print()
+    print(personKeys)
+    print()
+    dfPersonSectionSheet = pd.DataFrame(
+        personSectionSheet, columns=["CFN"] + [f"{i}" for i in personKeys]
+    )
+
+    dwellingKeys = list(
+        data[list(data.keys())[0]]["dwellingSection"][
+            list(data[list(data.keys())[0]]["dwellingSection"].keys())[0]
+        ].keys()
+    )
+    dfDwellingSectionSheet = pd.DataFrame(
+        dwellingSectionSheet, columns=["CFN"] + [f"{i}" for i in dwellingKeys]
+    )
+
+    feedbackKeys = list(data[list(data.keys())[0]]["feedbackSection"].keys())
+    dFfeedbackSectionSheet = pd.DataFrame(
+        feedbackSectionSheet, columns=["CFN"] + [f"{i}" for i in feedbackKeys]
+    )
+
+    excelPath = os.path.join(folder, "stats.xlsx")
+    with pd.ExcelWriter(excelPath, engine="xlsxwriter") as writer:
+        dfFirtsSheet.to_excel(writer, sheet_name="Sections", index=False)
+        dfAdressSectionSheet.to_excel(writer, sheet_name="AdressSection", index=False)
+        dfPersonSectionSheet.to_excel(writer, sheet_name="PersonSection", index=False)
+        dfDwellingSectionSheet.to_excel(
+            writer, sheet_name="DwellingSection", index=False
+        )
+        dFfeedbackSectionSheet.to_excel(
+            writer, sheet_name="FeedbackSection", index=False
+        )
+
     return excelPath
