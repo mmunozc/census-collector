7d666db47b4388e1d22d34a22618cc4a4ab40787
Hobar <huparelaa@eafit.edu.co>
2023-11-23 22:39:10 -0500

    feat: JWT added to localstorage

--- web_app/src/Components/Forms/AddressSection.js
+++ web_app/src/Components/Forms/AddressSection.js
@@ -1,58 +1,122 @@
-import React from 'react';
-import './AddressSection.css'
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import "./AddressSection.css";
+import { useNavigate } from "react-router-dom";
+import axios from "../../api/axios";
 
 const AddressSection = ({ onNext }) => {
-    let navigate = useNavigate();
-    function handleButtonClick() {
-        navigate('/dashboard');
-    }
-    return (
-        <div className="address-container">
-            <h2>Address Section</h2>
-            <div className="address-card">
-                <form>
-                    <div className="row">
-                        <div className="col-6">
-                            <div className="input-group">
-                                <label htmlFor="address">Full address</label>
-                                <input type="text" id="address" />
-                            </div>
-                        </div>
-                        <div className="col-6">
-                            <div className="input-group">
-                                <label htmlFor="addInformation"> Additional information</label>
-                                <input type="text" id="addInformation" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-4">
-                            <div className="input-group">
-                                <label htmlFor="city">City</label>
-                                <input type="text" id="city" />
-                            </div>
-                        </div>
-                        <div className="col-4">
-                            <div className="input-group">
-                                <label htmlFor="state">State</label>
-                                <input type="text" id="state" />
-                            </div>
-                        </div>
-                        <div className="col-4">
-                            <div className="input-group">
-                                <label htmlFor="postalCode">Postal code</label>
-                                <input type="text" id="postalCode" />
-                            </div>
-                        </div>
-                    </div>
+  let navigate = useNavigate();
 
-                    <button type="button" className="round-button" onClick={handleButtonClick}>Back</button>
-                    <button type="button" className="round-button mx-5" onClick={onNext}>Save</button>
-                </form>
+  // Crear estados para cada campo de entrada
+  const [address, setAddress] = useState("");
+  const [additionalInfo, setAdditionalInfo] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+
+  const handleSubmit = async () => {
+    try {
+      const addressData = {
+        address,
+        additionalInfo,
+        city,
+        state,
+        postalCode,
+      };
+
+      console.log(addressData);
+      // Aquí envías los datos al backend
+      await axios.post("/ruta-del-backend", addressData);
+
+      // Llama a onNext o realiza cualquier otra acción después de enviar los datos
+      onNext();
+    } catch (error) {
+      console.error("Error al enviar la dirección:", error);
+    }
+  };
+
+  return (
+    <div className="address-container">
+      <h2>Address Section</h2>
+      <div className="address-card">
+        <form>
+          <div className="row">
+            <div className="col-6">
+              <div className="input-group">
+                <label htmlFor="address">Full address</label>
+                <input 
+                  type="text" 
+                  id="address"
+                  value={address}
+                  onChange={e => setAddress(e.target.value)} 
+                />
+              </div>
             </div>
-        </div>
-    );
+            <div className="col-6">
+              <div className="input-group">
+                <label htmlFor="addInformation"> Additional information</label>
+                <input 
+                  type="text" 
+                  id="addInformation"
+                  value={additionalInfo}
+                  onChange={e => setAdditionalInfo(e.target.value)} 
+                />
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-4">
+              <div className="input-group">
+                <label htmlFor="city">City</label>
+                <input 
+                  type="text" 
+                  id="city"
+                  value={city}
+                  onChange={e => setCity(e.target.value)} 
+                />
+              </div>
+            </div>
+            <div className="col-4">
+              <div className="input-group">
+                <label htmlFor="state">State</label>
+                <input 
+                  type="text" 
+                  id="state"
+                  value={state}
+                  onChange={e => setState(e.target.value)} 
+                />
+              </div>
+            </div>
+            <div className="col-4">
+              <div className="input-group">
+                <label htmlFor="postalCode">Postal code</label>
+                <input 
+                  type="text" 
+                  id="postalCode"
+                  value={postalCode}
+                  onChange={e => setPostalCode(e.target.value)} 
+                />
+              </div>
+            </div>
+          </div>
+
+          <button
+            type="button"
+            className="round-button"
+            onClick={() => navigate("/dashboard")}
+          >
+            Back
+          </button>
+          <button
+            type="button"
+            className="round-button mx-5"
+            onClick={handleSubmit}
+          >
+            Save
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddressSection;
